05fdfb00c9b14b443663cd8783fefde4cf179536
Lea Pälkiö <lea.palkio@if.fi>
2023-11-14 12:37:29 +0200

    only create game if there no active games among the players

--- chessapp/views/new_game.py
+++ chessapp/views/new_game.py
@@ -6,8 +6,14 @@ from django.utils import timezone
 @login_required
 def new_game(request):
 	if request.method == 'POST':
-		game = Game(start_date = timezone.now())
 		opponent = User.objects.get(username = request.POST.get('opponent'))
+		existing_games1 = Game.objects.filter(white_player=request.user, black_player=opponent, end_date__isnull=True)
+		existing_games2 = Game.objects.filter(black_player=request.user, white_player=opponent, end_date__isnull=True)
+		if len(existing_games1) > 0 or len(existing_games2) > 0:
+			players = User.objects.all()
+			current_player = request.user
+			return render(request, 'chessapp/new_game.html', {"players": players, "current_player": current_player, "message": "please finnish a game with that player before starting a new one"})					
+		game = Game(start_date = timezone.now())		
 		if request.POST.get('color') == "w":
 			game.white_player = request.user
 			game.black_player = opponent
