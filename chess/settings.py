e640b0dfb39ef13f9faa65a44878dc94ae4bd2e3
Lea Pälkiö <lea.palkio@if.fi>
2023-11-24 17:04:43 +0200

    gitignore

--- chess/settings.py
+++ chess/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'myemail@gmail.com'
+EMAIL_HOST_PASSWORD = '*****'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Application definition
 
