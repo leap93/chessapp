82ab4e2f5fe9772097b642b21613460cc8df49ca
Lea Pälkiö <lea.palkio@if.fi>
2023-11-24 17:04:21 +0200

    gitignore

--- chessapp/views/registeration.py
+++ chessapp/views/registeration.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.db import IntegrityError
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-
+from django.core.mail import send_mail
+from chessapp.models import Confirmation
 def registeration(request):
 
 	if request.method == 'POST':
@@ -19,7 +20,21 @@ def registeration(request):
 		user.first_name = first_name
 		user.last_name = last_name
 		user.save()
-		return render(request, 'chessapp/register.html', {'message' : 'Registeration successful!'})
+		code = 123456
+		
+		confirmation = Confirmation.objects.create(user = user, confirmation_code = code)
+		
+		send_mail(
+			"Your confirmation code",
+			"Your confimration code is " + str(code),
+			"chessapp@chessapp.com",
+			[email],
+			fail_silently=False,
+		)
+		
+		
+		
+		return render(request, 'chessapp/email_confirmation.html', {'message' : 'Registeration successful!'})
 
 
 	if request.method == 'GET':
